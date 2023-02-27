dd732889deebd255a8ede1200295e5b97161fbaa
Dylan Hias <dylan.hias@student.ucll.be>
2023-02-27 17:04:03 +0100

    added navigation

--- App.js
+++ App.js
@@ -2,15 +2,21 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 // Import screens
-import Homescreen from './screens/Homescreen';
+import HomeScreen from './screens/HomeScreen';
+import HouseScreen from './screens/HouseScreen';
+import RoomScreen from './screens/RoomScreen';
+import DeviceScreen from './screens/DeviceScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Homescreen} options={{headerShown: false}}/>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false, animation: 'simple_push'}} />
+        <Stack.Screen name="House" component={HouseScreen} options={{headerShown: false, animation: 'simple_push'}} />
+        <Stack.Screen name="Room" component={RoomScreen} options={{headerShown: false, animation: 'simple_push'}} />
+        <Stack.Screen name="Device" component={DeviceScreen} options={{headerShown: false, animation: 'simple_push'}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
